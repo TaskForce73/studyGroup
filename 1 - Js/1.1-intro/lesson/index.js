77938e9fd2178435fc1e38e378819263d648c7d4
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-09-26 13:45:08 +0300

    Update index.js

--- 1 - Js/1.1-intro/lesson/index.js
+++ 1 - Js/1.1-intro/lesson/index.js
@@ -26,8 +26,128 @@
 // 1 and 2 // &&
 // 1 or 2 // ||
 
+//-----function-----
+// Regular
+// function fun1(qwe) {
+//     return {
+//         a: qwe
+//     }
+// }
 
+// console.log(fun1("rfgoehrg"))
+// Anonymous
+// () => {
 
+// }
 
+// (() => {
 
+// })()
 
+// Arrow
+// const xfun2 = () => {
+//     return 5
+// }
+
+// console.log(xfun2())
+
+//---if, else if, else---
+// const userData = prompt('eorignoer')
+
+// if (userData.length > 0) {
+//     alert(userData)
+// } else {
+//     alert('WE need more letters!!')
+// }
+
+//---Switch case----
+// switch (userData) {
+//     case "da":
+//         console.log('we get yes')
+//         break;
+//     case "net":
+//         console.log('we get no')
+//         break;
+//     default:
+//         console.log(`alse`)
+// }
+
+//---Loop---
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// const obj = {
+//     name: "Anton",
+//     hobbie: "Sleep"
+// }
+
+// for (key in obj) {
+//     console.log(key)
+// }
+
+// let i = 5;
+
+// while (i > 10) {
+//     console.log(i++)
+// }
+
+// let y = 7
+
+// do {
+//     console.log(y++)
+// } while (y > 10);
+
+//----OBJECTS----
+// const obj = {
+//     name: "Anton",
+//     hobbie: "Sleep",
+//     getHobbie: function () {
+
+//         function gethobbie222() {
+//             return this.hobbie
+//         }
+
+//         console.log(gethobbie222())
+//         return this.hobbie
+//     }
+// }
+
+// console.log(obj["name"])
+// const { name, hobbie } = obj
+// console.log(name, hobbie)
+
+// console.log(obj.getHobbie())
+
+// const stringify = JSON.stringify(obj)
+// console.log(stringify)
+
+// const aaa = {
+//     "name": "Anton",
+//     "hobbie": "Sleep"
+// }
+
+// const parseJson = JSON.parse(aaa)
+// console.log(parseJson)
+
+//-----OBJ OF STORE----
+const store = {
+    products: [
+        // {name:..., price...}
+    ],
+    addNewProduct: function (id, name, price) {
+        return this.products.push({ id, name, price })
+    },
+    deleteProduct: function (id) {
+        console.log(id)
+        // this.products
+        return this.products.filter((item) => item.id !== id)
+    }
+}
+
+store.addNewProduct(12, "apple", 789)
+store.addNewProduct(15, "banana", 789)
+// console.log(store.products)
+
+// console.log(store.products)
+console.log(store.deleteProduct(15))
