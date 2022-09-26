dd0f5925e6fa66180a6749d0b74b714646ebbf93
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-09-26 12:17:51 +0300

    start

---
+++ 1 - Js/1.1-intro/lesson/index.js
@@ -0,0 +1,33 @@
+//vars
+//number, boolean, str, bigint, undefind, NaN, null, obj, array
+
+// prompt() // field with input - string
+// alert() //alert - true
+// confirm() //boolean - true / false
+
+// const number = +prompt('enter number')
+// console.log(typeof number)
+
+// const str = '' , "", ``
+
+// console.log(++5)
+// let five = 5
+// console.log(five++)
+// console.log(++five)
+
+// let num1 = num1 + 5
+// let num1 = 5
+// num1 += 5
+
+// =
+// == //not strong - true if 5 == "5" - value
+// === //strong - false if 5==="5" - type and value
+
+// 1 and 2 // &&
+// 1 or 2 // ||
+
+
+
+
+
+
